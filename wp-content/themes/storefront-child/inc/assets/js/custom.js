ff131a382f51add7d3d928ae2274564e81f1067e
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-02-25 00:49:55 +0300

    скролл и меню

--- wp-content/themes/storefront-child/inc/assets/js/custom.js
+++ wp-content/themes/storefront-child/inc/assets/js/custom.js
@@ -2,9 +2,18 @@ jQuery(function ($) {
     const $page = $('html, body');
     $('a[href*="#"]').on('click', function () {
         let blockId = $.attr(this, 'href').slice(1)
+        $('.checkbox-toggle').trigger('click')
         $page.animate({
             scrollTop: $(`${blockId}`).offset().top - 50
-        }, 1000);
+        }, 1500);
         return false;
+    });
+
+    $('.checkbox-toggle').on('change', function () {
+        if ($('.checkbox-toggle').is(':checked')) {
+            $('body').addClass('overflow-hidden');
+        } else {
+            $('body').removeClass('overflow-hidden');
+        }
     });
 });
